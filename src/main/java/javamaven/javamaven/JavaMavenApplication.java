ea18653d545f5d6f0acf9b9af9b7ad0fa182cb32
Pawan Bahirat <pawanbahirat02@gmail.com>
2023-10-29 13:17:12 +0530

    initial commit

---
+++ src/main/java/javamaven/javamaven/JavaMavenApplication.java
@@ -0,0 +1,13 @@
+package javamaven.javamaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMavenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/javamaven/javamaven/JavaMavenApplicationTests.java
@@ -0,0 +1,13 @@
+package javamaven.javamaven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMavenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
