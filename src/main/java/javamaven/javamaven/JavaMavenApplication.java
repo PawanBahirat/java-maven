74aea4d7c2aa4a87a397939f3c31cad280bda9f4
Pawan Bahirat <pawanbahirat02@gmail.com>
2023-10-29 13:36:05 +0530

    second commit

--- src/main/java/javamaven/javamaven/JavaMavenApplication.java
+++ src/main/java/javamaven/javamaven/JavaMavenApplication.java
@@ -2,12 +2,22 @@ package javamaven.javamaven;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JavaMavenApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaMavenApplication.class, args);
+		
+		getData();
 	}
-
+	@GetMapping(value = "/get-data")
+	public static Object getData() {
+	    return new Object() {
+	        public String name = "pawan";
+	    };
+	}
 }
